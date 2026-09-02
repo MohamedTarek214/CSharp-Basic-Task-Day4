f307d9722598d8ff11426d2eb0a62d41d503f575
MhmedTarekRadwan <mhmedtarekradwan@gmail.com>
2026-09-02 19:58:51 +0300

    Solving the first problem

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -4,8 +4,36 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Problem1 
+            //int[] Numbers1 = new int[4]; // 1
+            //Numbers1[0] = 1;
+            //Numbers1[1] = 2;
+            //Numbers1[2] = 3;
+            //Numbers1[3] = 4;
+            //int[] Numbers2 = new int[4] { 1, 2, 3, 4 }; // 2
+            //int[] Numbers3 = { 1, 2, 3, 4 }; // 3
+            //Console.WriteLine("first arry");
+            //foreach (var item in Numbers1)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //Console.WriteLine("second arry");
+            //foreach (var item in Numbers2)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //Console.WriteLine("third arry");
+            //foreach (var item in Numbers3)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //// IndexOutOfRangeException. 
+            //int[] Numbers4 = { 1, 2, 3 };
+            //Console.WriteLine(Numbers4[3]);
 
+            #endregion
+
+            
 
         }
     }
