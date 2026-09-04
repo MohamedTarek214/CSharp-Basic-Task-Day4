98bdad76c38e0b18e002c0869e80cf2d9c7d3bf3
MhmedTarekRadwan <mhmedtarekradwan@gmail.com>
2026-09-04 16:32:28 +0300

    Solve Some Problem

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -65,7 +65,106 @@
             //Console.WriteLine(arr2[2]); 
             #endregion
 
-            
+            #region Problem3  
+            //int[,] StudentGrades = new int[3, 3];
+            //for (int i = 0; i < StudentGrades.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < StudentGrades.GetLength(1); j++)
+            //    {
+            //        Console.Write($"Enter grade for student {i + 1}, subject {j + 1}: ");
+            //        StudentGrades[i, j] = int.Parse(Console.ReadLine());
+            //    }
+            //}
+            //for (int i = 0; i < StudentGrades.GetLength(0); i++)
+            //{
+            //    Console.Write($"Grades for student {i + 1}: ");
+            //    for (int j = 0; j < StudentGrades.GetLength(1); j++)
+            //    {
+            //        Console.Write(StudentGrades[i, j] + " ");
+            //    }
+            //    Console.WriteLine();
+            //} 
+            #endregion
+
+            #region Problem4 
+            //int[] arr = { 5, 4, 3, 2, 1 };
+            //Console.WriteLine("Before Sort");
+            //foreach (var item in arr)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //Console.WriteLine("--------------------");
+            //Array.Sort(arr);
+            //Console.WriteLine("After Sort");
+            //foreach (var item in arr)
+            //{
+            //    Console.WriteLine(item);
+
+            //}
+            //Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            //Console.WriteLine("Before Reverse");
+            //foreach (var item in arr)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //Console.WriteLine("--------------------");
+            //Array.Reverse(arr);
+            //Console.WriteLine("After Reverse");
+            //foreach (var item in arr)
+            //{
+            //    Console.WriteLine(item);
+
+            //}
+            //Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            //Console.WriteLine("Before IndexOf");
+            //foreach (var item in arr)
+            //{
+            //    Console.WriteLine(item);
+
+            //}
+            //Console.WriteLine("--------------------");
+            //Console.WriteLine("After IndexOf");
+            //int num =  Array.IndexOf(arr, 3);
+            //Console.WriteLine(num);
+            //Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            //Console.WriteLine("Before Clear");
+            //foreach (var item in arr)
+            //{
+            //    Console.WriteLine(item);
+
+            //}
+            //Console.WriteLine("--------------------");
+            //Console.WriteLine("After Clear");
+            //Array.Clear(arr);
+
+            #endregion
+
+            #region Problem5
+            //string[] Names = { "Ali", "Ahmed", "Mohamed", "Hassan" };
+            //Console.WriteLine("print All Elements By for loops ");
+            //for (int i = 0; i < Names.Length; i++)
+            //{
+            //    Console.WriteLine(Names[i]);
+
+            //}
+            //Console.WriteLine("======================================");
+            //Console.WriteLine("print All Elements By foreach loops ");
+            //foreach (var item in Names)
+            //{
+            //    Console.WriteLine(item);
+
+
+            //}
+            //Console.WriteLine("======================================");
+            //Console.WriteLine("print All Elementsin reverse order By while loops ");
+            //int index = (Names.Length - 1);
+            //while (index >= 0)
+            //{
+            //    Console.WriteLine(Names[index]);
+            //    index--;
+            //} 
+            #endregion
+
 
         }
     }
