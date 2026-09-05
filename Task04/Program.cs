8da74bce2192c1a30011c75b3e4b989a787fc31c
MhmedTarekRadwan <mhmedtarekradwan@gmail.com>
2026-09-05 11:16:17 +0300

    Finshing Problem

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -165,7 +165,177 @@
             //} 
             #endregion
 
+            #region Problem6 
+            //int PositiveOddNumber;
+            //bool Flage;
+            //do
+            //{
+            //    Console.WriteLine("Enter a positive odd number");
+            //    Flage = int.TryParse(Console.ReadLine(), out PositiveOddNumber);
 
+            //}
+            //while (PositiveOddNumber <= 0 || PositiveOddNumber % 2 == 0 || !Flage);
+            //Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~");
+            //Console.WriteLine($"positive odd number: {PositiveOddNumber}");
+            //Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~"); 
+            #endregion
+
+            #region Problem7  
+            //int[,] numbers =
+            //{
+            //    { 1, 2, 3 },
+            //    { 4, 5, 6 },
+            //    { 7, 8, 9 }
+            //};
+
+            //for (int i = 0; i < numbers.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < numbers.GetLength(1); j++)
+            //    {
+            //        Console.Write(numbers[i, j] + " ");
+            //    }
+
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+            #region Problem8  
+            //Console.Write("Enter month number: ");
+            //int month = int.Parse(Console.ReadLine());
+
+            //    if (month == 1)
+            //        Console.WriteLine("January");
+            //    else if (month == 2)
+            //        Console.WriteLine("February");
+            //    else if (month == 3)
+            //        Console.WriteLine("March");
+            //    else if (month == 4)
+            //        Console.WriteLine("April");
+            //    else if (month == 5)
+            //        Console.WriteLine("May");
+            //    else if (month == 6)
+            //        Console.WriteLine("June");
+            //    else if (month == 7)
+            //        Console.WriteLine("July");
+            //    else if (month == 8)
+            //        Console.WriteLine("August");
+            //    else if (month == 9)
+            //        Console.WriteLine("September");
+            //    else if (month == 10)
+            //        Console.WriteLine("October");
+            //    else if (month == 11)
+            //        Console.WriteLine("November");
+            //    else if (month == 12)
+            //        Console.WriteLine("December");
+            //    else
+            //        Console.WriteLine("Invalid month");
+            //Console.WriteLine("***************************************");
+
+            //switch (month)
+            //{
+            //    case 1:
+            //        Console.WriteLine("January");
+            //        break;
+
+            //    case 2:
+            //        Console.WriteLine("February");
+            //        break;
+
+            //    case 3:
+            //        Console.WriteLine("March");
+            //        break;
+
+            //    case 4:
+            //        Console.WriteLine("April");
+            //        break;
+
+            //    case 5:
+            //        Console.WriteLine("May");
+            //        break;
+
+            //    case 6:
+            //        Console.WriteLine("June");
+            //        break;
+
+            //    case 7:
+            //        Console.WriteLine("July");
+            //        break;
+
+            //    case 8:
+            //        Console.WriteLine("August");
+            //        break;
+
+            //    case 9:
+            //        Console.WriteLine("September");
+            //        break;
+
+            //    case 10:
+            //        Console.WriteLine("October");
+            //        break;
+
+            //    case 11:
+            //        Console.WriteLine("November");
+            //        break;
+
+            //    case 12:
+            //        Console.WriteLine("December");
+            //        break;
+
+            //    default:
+            //        Console.WriteLine("Invalid month");
+            //        break;
+            //}
+
+            #endregion
+
+            #region Problem9 
+
+            //int[] numbers = { 5, 2, 8, 2, 9, 3, 2 };
+
+            //Array.Sort(numbers);
+
+            //Console.WriteLine("Sorted array:");
+
+            //foreach (int number in numbers)
+            //{
+            //    Console.Write(number + " ");
+            //}
+
+            //Console.WriteLine();
+
+            //Console.Write("Enter a value to search: ");
+            //int value = int.Parse(Console.ReadLine());
+
+            //int firstIndex = Array.IndexOf(numbers, value);
+            //int lastIndex = Array.LastIndexOf(numbers, value);
+
+            //Console.WriteLine("First index: " + firstIndex);
+            //Console.WriteLine("Last index: " + lastIndex); 
+            #endregion
+
+            #region Problem10 
+            //int[] numbers = { 10, 20, 30, 40 };
+
+            //int SumFor = 0;
+
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    SumFor += numbers[i];
+            //}
+
+            //Console.WriteLine("Sum using for: " + SumFor);
+
+
+            //int SumForeach = 0;
+
+            //foreach (int number in numbers)
+            //{
+            //    SumForeach += number;
+            //}
+
+            //Console.WriteLine("Sum using foreach: " + SumForeach); 
+            #endregion
         }
     }
 }
